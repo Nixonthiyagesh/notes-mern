20fa4cb5f3ce8684e837b4f8fd87278558f71010
Nixon <nixgesh@gmail.com>
2024-09-20 08:49:41 +0530

    api url

--- notes-frontend/src/axios.js
+++ notes-frontend/src/axios.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const baseURL =
-  process.env.ENVIRONMENT === "production"
-    ? process.env.REACT_APP_API_URL
-    : "http://localhost:4000";
+const baseURL = process.env.REACT_APP_API_URL;
 
 export const axiosInstance = axios.create({
   baseURL,
